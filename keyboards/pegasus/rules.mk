c535ee02ec3e66255ee0ce2e0e138828d44933a5
melonbred <61572569+melonbred@users.noreply.github.com>
2021-03-09 18:22:45 -0800

    f042 firmware

---
+++ keyboards/pegasus/rules.mk
@@ -0,0 +1,26 @@
+# MCU name
+MCU = STM32F042
+
+# Build Options
+#   comment out to disable the options.
+#
+
+BOOTMAGIC_ENABLE = no	# Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no	# Mouse keys
+EXTRAKEY_ENABLE = yes	# Audio control and System control
+CONSOLE_ENABLE = no	# Console for debug
+COMMAND_ENABLE = no    # Commands for debug and configuration
+SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
+NKRO_ENABLE = no	    # USB Nkey Rollover
+CUSTOM_MATRIX = no # Custom matrix file
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = no
+WS2812_DRIVER = no
+ENCODER_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+
+
+
+
+# Enter lower-power sleep mode when on the ChibiOS idle thread
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
